a6c7d6b3a07741a9c6d5120bf34ab892ea0a26f6
Marcus842 <60646137+Marcus842@users.noreply.github.com>
2024-10-04 15:33:11 +0200

    Formatted document
    Formatted document.
    Added more error handling for better feedback.
    Added conditional rendering for previous and next button

--- src/App.js
+++ src/App.js
@@ -1,81 +1,106 @@
 import './style.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Movie from './Movie';
 
 function App() {
-  const[movieElements,setMovieElements]=useState(null);
-  const[title,setTitle]=useState("");
-  const[pageIndex,setPageIndex]=useState(1);
-  const[searchResultsNumber,setSearchResultsNumber]=useState(null);
-  const apiKey=process.env.REACT_APP_API_KEY;
+  const [movieElements, setMovieElements] = useState(null);
+  const [title, setTitle] = useState("");
+  const [pageIndex, setPageIndex] = useState(1);
+  const [searchResultsNumber, setSearchResultsNumber] = useState(null);
+  const [displayPrevButton, SetDisplayPrevButton] = useState(false);
+  const [displayNextButton, SetDisplayNextButton] = useState(false);
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-  async function handleClick(event){
+  async function handleClick(event) {
     event.preventDefault();
 
     let queryPageIndex;
-    if(event.target.id==="next")
-    {
-      queryPageIndex=pageIndex+1;
+    if (event.target.id === "next") {
+      queryPageIndex = pageIndex + 1;
       setPageIndex(queryPageIndex);
     }
-    else if(event.target.id==="previous")
-      {
-        queryPageIndex=pageIndex-1;
-        setPageIndex(queryPageIndex);
-      }
-      else{
-        queryPageIndex=pageIndex;
-      }
+    else if (event.target.id === "previous") {
+      queryPageIndex = pageIndex - 1;
+      setPageIndex(queryPageIndex);
+    }
+    else {
+      queryPageIndex = pageIndex;
+    }
 
-    try{
-      const res=await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${title}&page=${queryPageIndex}`);
-      const resData=await res.json();
-      if(!res.ok){
+    let totalResults;
+    try {
+      const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${title}&page=${queryPageIndex}`);
+      const resData = await res.json();
+      if (!res.ok) {
         console.error(`Error status: ${res.status}`);
       }
-      else{
-        setSearchResultsNumber(resData.totalResults);
-        const movies=resData.Search.map(movie=>
+      else if (resData.Response === "False") {
+        console.error(`Error message: ${resData.Error}`);
+      }
+      else {
+        totalResults = resData.totalResults;
+        setSearchResultsNumber(totalResults);
+        const movies = resData.Search.map(movie =>
           <Movie img={movie.Poster} key={movie.imdbID} title={movie.Title} year={movie.Year} type={movie.Type}></Movie>
         );
         setMovieElements(movies);
       }
     }
-    catch(error)
-    {
+    catch (error) {
       console.error(error);
+    }
+    if (queryPageIndex > 1) {
+      SetDisplayPrevButton(true);
+    }
+    else {
+      SetDisplayPrevButton(false);
+    }
+    if (totalResults) {
+      var totalResultsInt = parseInt(totalResults);
+      var numberOfPages = totalResultsInt / 10;
+      var numberOfPagesInt = Math.ceil(numberOfPages);
+      if (queryPageIndex < numberOfPagesInt) {
+        SetDisplayNextButton(true);
+      }
+      else {
+        SetDisplayNextButton(false);
+      }
+    }
+    else {
+      SetDisplayNextButton(false);
     }
   }
 
-  function handleChange(e){
-    const val=e.target.value
-    if(val != null)
-    {
-      setTitle(val)
-    }
- }
+  function handleChange(e) {
+    const val = e.target.value
+    setTitle(val)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome</h1>
-        <form onSubmit = {handleClick}>
-                    <input type = 'text' placeholder = 'Movie title' name="title" value={title} onChange={handleChange}></input>
-                    <input type = 'submit' placeholder='Submit' value = 'Submit'></input>
+        <h1>Welcome</h1>
+        <form onSubmit={handleClick}>
+          <input type='text' placeholder='Movie title' name="title" value={title} onChange={handleChange}></input>
+          <input type='submit' placeholder='Submit' value='Submit'></input>
         </form>
         <div>
-          <div className='pagination-div-class'>
-            <form onSubmit = {handleClick} id="previous" name='previous'>
-                    <input type = 'submit' placeholder='Previous' value = 'Previous'></input>
-            </form>
+          {displayPrevButton ?
+            (<div className='pagination-div-class'>
+              <form onSubmit={handleClick} id="previous" name='previous'>
+                <input type='submit' placeholder='Previous' value='Previous'></input>
+              </form>
+            </div>) :
+            null}
+          {displayNextButton ?
+            (<div className='pagination-div-class'>
+              <form onSubmit={handleClick} id="next" name='next'>
+                <input type='submit' placeholder='Next' value='Next'></input>
+              </form>
+            </div>) :
+            null}
         </div>
-        <div className='pagination-div-class'>
-            <form onSubmit = {handleClick} id="next" name='next'>
-                    <input type = 'submit' placeholder='Next' value = 'Next'></input>
-            </form>
-          </div>
-        </div>
-        {searchResultsNumber ? (<p ><b>Number of search results: </b>{searchResultsNumber}</p>) : null}   
+        {searchResultsNumber ? (<p ><b>Number of search results: </b>{searchResultsNumber}</p>) : null}
       </header>
 
       <div className='App-content'>
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,12 @@
-export default function Movie(props){
-    const imageText=props.title+" image text";
+export default function Movie(props) {
+    const imageText = props.title + " image text";
     return (
-            <div>
-                {props.img && props.img !== "N/A" ? (<img className="poster-image" src={props.img} alt={imageText}/>) : null}                
-                <ul className="image-spec-ul">
-                    <li><b>Title: </b>{props.title}</li>
-                    <li><b>Year: </b>{props.year}</li>
-                    <li><b>Type: </b>{props.type}</li>
-                </ul>
-            </div>);
+        <div>
+            {props.img && props.img !== "N/A" ? (<img className="poster-image" src={props.img} alt={imageText} />) : null}
+            <ul className="image-spec-ul">
+                <li><b>Title: </b>{props.title}</li>
+                <li><b>Year: </b>{props.year}</li>
+                <li><b>Type: </b>{props.type}</li>
+            </ul>
+        </div>);
 }
